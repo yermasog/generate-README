ae5857b8d8ab67bf02a952e9b4ecf3db6896bff3
ymasog <yer.masog@gmail.com>
2021-01-26 16:14:55 -0600

    fixed writefile function and readme is generated

---
+++ Develop/README.md
@@ -0,0 +1,31 @@
+
+      # fghj
+
+
+      ## Description
+      gfg
+      
+      
+      ## Installation
+      jhgfbv
+      
+      
+      ## Usage
+      gdfxgh
+      
+      
+      ## License
+      b
+      
+      
+      ## Contributing
+      sfghh
+      
+      
+      ## Tests
+      trgdfg
+      
+      
+      ## Questions
+      regd
+      utsjh
--- Develop/index.js
+++ Develop/index.js
@@ -1,80 +1,93 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-// TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        message: 'What is your project title?',
-        name: 'project title',
-      },
-      {
-        type: 'input',
-        message: 'Enter a short description of the project.',
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: 'Enter installation instructions.',
-        name: 'installation',
-      },
-      {
-        type: 'input',
-        message: 'Enter usage instructions.',
-        name: 'usage',
-      },
-      {
-        type: 'input',
-        message: 'What type of license do you want?',
-        name: 'license',
-      },
-      {
-        type: 'input',
-        message: 'what are the contribution guidelines?',
-        name: 'contributing',
-      },
-      {
-        type: 'input',
-        message: 'What are the testing instructions?',
-        name: 'tests',
-      },
-      {
-        type: 'input',
-        message: 'What is your GitHub username?',
-        name: 'username',
-      },
-      {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-      },
-];
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "title"
+  },
+  {
+    type: "input",
+    message: "Enter a short description of your project.",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation"
+  },
+  {
+    type: "input",
+    message: "Enter usage instructions?",
+    name: "usage"
+  },
+  {
+    type: "list",
+    message: "What kind of license do you want?",
+    choices: ["a", "b", "c"],
+    name: "license"
+  },
+  {
+    type: "input",
+    message: "What are the guidelines for contributing?",
+    name: "contributing"
+  },
+  {
+    type: "input",
+    message: "What are the testing options?",
+    name: "testing"
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github"
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email"
+  },
 
-// TODO: Create a function to write README file
-fs.writeFile("README.md", "hello world", (err) => {
-    if(err) {
-        console.log(err);
+
+]
+
+inquirer.prompt(questions).then(response =>
+// console.log(response),
+
+{
+  let readme = `
+      # ${response.title}
+      
+      ## Description
+      ${response.description}
+      
+      ## Installation
+      ${response.installation}
+      
+      ## Usage
+      ${response.usage}
+      
+      ## License
+      ${response.license}
+      
+      ## Contributing
+      ${response.contributing}
+      
+      ## Tests
+      ${response.testing}
+      
+      ## Questions
+      ${response.github}
+      ${response.email}`;
+
+  fs.writeFile("README.md", readme, (err) => {
+    if (err) {
+      console.log(err);
     }
     else {
-        console.log("success")
+      console.log("created file");
     }
-})
-    
-  
-
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then((response) =>
-    console.log(response),
-    writeFile()
- 
-  );
-    
+  })
 }
-
-// Function call to initialize app
-init();
+);
 
